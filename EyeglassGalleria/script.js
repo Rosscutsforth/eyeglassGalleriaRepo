16a73ac7c9e26eff17f74d710d33e66e8758d8a8
Rosscutsforth <Rosscutsforth@gmail.com>
2020-09-14 10:22:31 -0700

    Added all pages and wireframe
    Added all main pages and filled them out with a wireframe. Completed header as well

---
+++ EyeglassGalleria/script.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){
+  // HEADER Script
+
+  var prevScrollPos = window.pageYOffset;
+
+  var sticky = header.offsetTop;
+
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollPos > currentScrollPos) {
+      document.getElementById("header").style.top = "0";
+      header.classList.add('sticky');
+      } else {
+      document.getElementById("header").style.top = "-78px";
+    }
+    prevScrollPos = currentScrollPos;
+    if (window.pageYOffset <= 30) {
+      header.classList.remove('sticky');
+      document.getElementById('header').style.top = '0';
+    };
+  };
+});
